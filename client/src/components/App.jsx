ef58f167b562c439708d7cb884f1187d672001a0
Milos Bunijevac <milos.bunijevac@gmail.com>
2017-04-26 13:10:21 -0700

    "added App component and webpack works"

---
+++ client/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2> This is the app component </h2>
+      </div>
+    );
+  }
+}
+
+export default App;
