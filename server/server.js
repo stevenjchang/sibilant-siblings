6185cd5772cde3152657b438b02b68124faf1f79
Steve <stevenjchang@gmail.com>
2017-04-26 15:18:14 -0700

    Implement morgan, added port variable

--- server/server.js
+++ server/server.js
@@ -1,15 +1,22 @@
 var express = require('express')
 var app = express()
-var bodyparser = require('body-parser');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var port = process.env.PORT || 3000;
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use (morgan("dev"));
 
 app.get('/', function (req, res) {
-  res.send('get request received');
+  res.send('GET request received inside server.js');
 })
 
 app.post('/', function (req, res) {
-  res.send('post request received');
+  console.log('*** req.body *** >server.js --> ', req.body);
+  // res.send('POST request received inside server.js');
+  res.send(req.body);
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
 })
