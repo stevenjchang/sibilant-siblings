b5b1aa94fe0f1fe223cf0a5e56c62d67396a2065
Steve <stevenjchang@gmail.com>
2017-05-09 15:37:29 -0700

    Add apisecrets.js, add apisecrets.js to .gitignore, replaced token with require apisecrets inside apicall.js

--- apicall.js
+++ apicall.js
@@ -9,26 +9,10 @@ const searchRequest = {
   limit: 1
 };
 
-// yelp.accessToken(ApiSecrets.clientId, ApiSecrets.clientSecret).then(response => {
-//   const client = yelp.client(response.jsonBody.access_token);
-
-//   client.search(searchRequest).then(response => {
-//     const allResults = response.jsonBody.businesses; //this will put all businesses in the location above into allresults
-//     const prettyJson = JSON.stringify(allResults, null, 4);
-//     console.log(prettyJson);
-//   });
-// }).catch(e => {
-//   console.log(e);
-// });
-
-//insert token here
-
-const token = '';
-
 const ApiCall = function(req, res) {
   const location = req.body.zip ? req.body.zip : 94105;
   const categories = req.body.categories ? req.body.categories : 'italian';
-  const client = yelp.client(token);
+  const client = yelp.client(ApiSecrets.yelpToken);
   
   client.search({
     location: location,
