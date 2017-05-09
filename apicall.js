69cb3eacbf86a1d08d6c70921c448a71b79209f2
Steve <stevenjchang@gmail.com>
2017-05-09 16:57:32 -0700

    Add getRestaurantsFromYelp function, Remove ApiCall function and yelp.accessToken function

--- apicall.js
+++ apicall.js
@@ -1,40 +1,22 @@
 const yelp = require('yelp-fusion');
 const ApiSecrets = require('./apisecrets.js');
+const token = ApiSecrets.yelpToken;
 
-const searchRequest = {
-  term: 'restaurant chinese vegetarian',
-  location: '94131',
-  radius: 8000,
-  price: '1,2,3,4',
-  limit: 1
-};
+let getRestaurantsFromYelp = function(userPref, callback) {
+  const id = 1; //hardcoding id for 1st user
+  const location = userPref[0].location ? userPref[0].location : 94105;
+  const preferences = userPref[0].preferences ? userPref[0].preferences : "italian, mexican";
+  const client = yelp.client(token);
 
-const ApiCall = function(req, res) {
-  const location = req.body.zip ? req.body.zip : 94105;
-  const categories = req.body.categories ? req.body.categories : 'italian';
-  const client = yelp.client(ApiSecrets.yelpToken);
-  
   client.search({
     location: location,
-    categories: categories
+    term: preferences
   }).then(response => {
-    allResults = response.jsonBody.businesses;
-    res.send(allResults.slice(0, 3));
+    let allResults = response.jsonBody.businesses;
+    callback(null, allResults);
   }).catch(e => {
-    res.send(e);
-  });
-};
+    callback(e, null);
+  })
+}
 
-yelp.accessToken(ApiSecrets.clientId, ApiSecrets.clientSecret).then(response => {
-  const client = yelp.client(response.jsonBody.access_token);
-  console.log('access_token: ', response.jsonBody.access_token);
-  client.search(searchRequest).then(response => {
-    const allResults = response.jsonBody.businesses; //this will put all businesses in the location above into allresults
-    const prettyJson = JSON.stringify(allResults, null, 4);
-    console.log(prettyJson);
-  });
-}).catch(e => {
-  console.log(e);
-});
-
-module.exports = ApiCall;
+module.exports.getRestaurantsFromYelp = getRestaurantsFromYelp;
