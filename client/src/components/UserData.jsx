63361eb0238d6b5bf01e0477ca6102fc0213d113
Milos Bunijevac <milos.bunijevac@gmail.com>
2017-05-11 00:26:51 -0700

    "reactive state working in userdata"

--- client/src/components/UserData.jsx
+++ client/src/components/UserData.jsx
@@ -3,18 +3,26 @@ import React from 'react';
 class UserData extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {username: 'Enter User', password: 'Enter Password', preferences: 'Italian', location: '94016'};
   }
+
+  updateVal(name, event) {
+    var updater = {};
+    updater[name] = event.target.value;
+    this.setState(updater);
+  }
+
   render() {
     return (
       <div>
         <p>Username</p>
-        <input name="username" defaultValue="Enter User"></input>
+        <input name="username" value={this.state.username} onChange={this.updateVal.bind(this, 'username')}></input>
         <p>Password</p>
-        <input name="password" defaultValue="Enter Password"></input>
+        <input name="password" value={this.state.password} onChange={this.updateVal.bind(this, 'password')}></input>
         <p>Preferences</p>
-        <input name="preferences" defaultValue="Italian"></input>
+        <input name="preferences" value={this.state.preferences} onChange={this.updateVal.bind(this, 'preferences')}></input>
         <p>Zip</p>
-        <input name="location" defaultValue="94016"></input>
+        <input name="location" value={this.state.location} onChange={this.updateVal.bind(this, 'location')}></input>
       </div>
     );
   }
