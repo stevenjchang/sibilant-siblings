d93d5712458973d51721e6de3d92f152cd7165b8
Adisa Stewart <adisa.stewart1988@gmail.com>
2017-05-15 15:00:02 -0700

    Update dbconnect.js

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -5,7 +5,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: '127.0.0.1',
   user: 'root',
-  password: 'IloveMySQL.',
+  password: '',
   database: 'foodQuest',
   multipleStatements: true
 });
