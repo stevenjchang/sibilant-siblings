5e5c3a26c263ef4cfee3d89cdcfb99b1f02a8556
Steve <stevenjchang@gmail.com>
2017-05-15 15:53:14 -0700

    add div id input-form

--- client/src/components/UserData.jsx
+++ client/src/components/UserData.jsx
@@ -8,7 +8,7 @@ class UserData extends React.Component {
 
   render() {
     return (
-      <div>
+      <div id='input-form'>
         <label>Username</label>
         <br />
         <input disabled name="username" value={this.props.allState.username} onChange={this.props.updateVal.bind(this, 'username')}></input>
@@ -24,6 +24,7 @@ class UserData extends React.Component {
         <label>Zip Code</label>
         <br />
         <input name="location" value={this.props.allState.location} onChange={this.props.updateVal.bind(this, 'location')}></input>
+        <br />
       </div>
     );
   }
